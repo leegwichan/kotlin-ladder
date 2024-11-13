71b83ed280607168772873f2c25be44db7fb20a8
unifolio0 <boho82ha@gmail.com>
2024-11-13 11:32:53 +0900

    feat: 사다리 높이 입력 기능

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -3,7 +3,13 @@ package ladder.view
 class InputView {
 
     fun inputNames(): List<String> {
+        println("참여할 사람 이름을 입력하세요. (이름은 쉼표(${NAME_DELIMITER})로 구분하세요)")
         return readln().split(NAME_DELIMITER)
+    }
+
+    fun inputLadderHeight(): Int {
+        println("\n최대 사다리 높이는 몇 개인가요?")
+        return readln().toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해야 합니다")
     }
 
     companion object {

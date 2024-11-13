ab344cca6ce4f38ca48fe4861954b14f2de139d2
unifolio0 <boho82ha@gmail.com>
2024-11-13 12:08:29 +0900

    feat: 참여자 중복 처리 기능

--- src/main/kotlin/domain/Name.kt
+++ src/main/kotlin/domain/Name.kt
@@ -1,6 +1,7 @@
 package ladder.domain
 
-class Name(val name: String) {
+data class Name(val name: String) {
+
     init {
         require(name.isNotBlank()) { "이름은 공백으로만 이루어 질 수 없습니다" }
         require(name.length <= MAX_NAME_LENGTH) { "이름은 ${MAX_NAME_LENGTH}를 넘을 수 없습니다" }
--- src/main/kotlin/domain/Names.kt
+++ src/main/kotlin/domain/Names.kt
@@ -1,8 +1,13 @@
 package ladder.domain
 
-class Names(names: List<Name>) {
+class Names(private val names: List<Name>) {
     init {
         require(names.isNotEmpty()) { "참여자는 1명 이상 있어야 합니다" }
+        require(names.distinct().size == names.size) { "참여자는 중복 될 수 없습니다" }
+    }
+
+    fun getSize(): Int {
+        return names.size
     }
 
     companion object {
